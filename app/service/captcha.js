d7864d65dab1109e8c2db328c12ce85c4ffb7311
王力宇 <915153147@qq.com>
2018-12-18 18:25:44 +0800

    完成邮件发送

--- app/service/captcha.js
+++ app/service/captcha.js
@@ -5,9 +5,10 @@ const Service = require('egg').Service;
 class CaptchaService extends Service {
 
   async getCode() {
-    // 创建单个数据直接传入数据模型obj即可，如果想要创建多个数据可以直接传入数组 [{},{}]
-    // 参考https://mongoosejs.com/docs/api.html#model_Model.createCollection
-    // const result = await this.ctx.model.User.create(data);
+    const code = await this.ctx.model.captcha.create({
+      code: '12345',
+    });
+    console.log('code', code);
     await this.ctx.service.email.send('123456');
     return null;
   }
--- app/service/email.js
+++ app/service/email.js
@@ -9,8 +9,8 @@ const transporter = nodemailer.createTransport({
   secure: true, // true for 465, false for other ports
   auth: {
     user: '915153147@qq.com', // generated ethereal user
-    // pass: 'qhcakidpntafbbaf', // generated ethereal password
-    pass: 'wlyisking1010',
+    pass: 'qhcakidpntafbbaf', // generated ethereal password
+    // pass: 'wlyisking1010',
   },
 });
 
@@ -19,11 +19,11 @@ class EmailService extends Service {
   async send(data) {
     // setup email data with unicode symbols
     const mailOptions = {
-      from: '这是王力宇发送的测试邮件', // sender address
+      from: '915153147@qq.com', // sender address
       to: 'wlyisking@2980.com', // list of receivers
-      subject: 'Hello', // Subject line
-      text: 'Hello world?' + data, // plain text body
-      html: '<b>Hello world?</b>', // html body
+      subject: '王力宇的博客的验证码', // Subject line
+      // text: '尊敬的' + data.name, // plain text body
+      html: '<b>您的验证码是: <h1>' + data + '</h1></b>', // html body
     };
 
     // send mail with defined transport object
